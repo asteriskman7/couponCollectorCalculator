d8146b9d44983535bdcb15fb86b89c07100877a0
asteriskman <asteriskman7@users.noreply.github.com>
2024-11-01 14:08:46 -0400

    fix stddev and add approximations for higher n

--- index.js
+++ index.js
@@ -26,6 +26,43 @@ class App {
     this.calculateBits();
   }
 
+  getHarmonic(n) {
+    let result;
+    //if n is small, calculate directly, otherwise, use approximation
+    if (n < 60) {
+      result = 0;
+      for (let i = 1; i <= n; i++) {
+        result += 1 / i;
+      }
+    } else {
+      const gamma = 0.5772156649 //euler-mascheroni constant
+      result = (Math.log(n) + gamma);
+    }
+    return result;
+  }  
+
+  getExpectedTries(n) {
+    //https://brilliant.org/wiki/coupon-collector-problem/
+    return Math.round(this.getHarmonic(n) * n);
+  }  
+
+  getStdDev(n) {
+    //https://brilliant.org/wiki/coupon-collector-problem/
+    let variance;
+    if (n < 60) {
+      variance = 0;
+      for (let i = 1; i <= n; i++) {
+        variance += 1 / (i * i);
+      }
+      variance *= n * n;
+      variance -= n * this.getHarmonic(n);
+    } else {
+      const gamma = 0.5772156649 //euler-mascheroni constant
+      variance = (Math.PI * Math.PI * n * n / 6) - n * (Math.log(n) + gamma) - 1/2;
+    }
+    return Math.sqrt(variance);
+  }
+
   calculateStates(writeBits) {
     const n = parseInt(this.UI.tstates.value);
     if (writeBits) {
@@ -33,25 +70,10 @@ class App {
       this.UI.tbits.value = bits.toFixed(2);
     }
  
-    //TODO: increase this limit...somehow
-    if (n > 134217728) {
-      this.UI.stries.innerText = "ERR: too large to calculate here";
-      return;
-    }
-
-    let expectedTries = 0;
-    for (let i = 1; i <= n; i++) {
-      expectedTries += 1 / i;
-    }
-    expectedTries *= n;
+    const expectedTries = this.getExpectedTries(n);
     this.UI.stries.innerText = expectedTries.toFixed(1);
 
-    let variance = 0;
-    for (let i = 1; i < n; i++) {
-      variance += i / (n - i);
-    }
-
-    const stddev = Math.sqrt(variance);
+    const stddev = this.getStdDev(n);
     this.UI.sstddev.innerText = stddev.toFixed(2);
 
     const table = document.createElement('table');  
